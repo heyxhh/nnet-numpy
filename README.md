ae064c27508c9bce3c174cbb5074d2ed6fbc55f6
haihua <x_haihua@163.com>
2020-03-03 22:19:04 +0800

    SGD增加动量梯度下降支持

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 
 - Linear、CNN、Maxpool、BatchNorm、Dropout、SoftmaxLoss等，正向反向传播的实现。
 - 代码包含大量的中文注释，对于刚接触深度学习的同学可以作为参考，用来当玩具练习。
-- 代码测试有效，设计了类似AlexNet的简单卷积网络，使用mnist数据集训练3个epoch后test accuracy(97%)。
-- 实验效果查看[net-test-mist.ipynb](https://github.com/heyxhh/nnet-numpy/blob/master/net-test-mnist.ipynb)
+- 代码测试有效，设计了类似AlexNet的简单卷积网络，使用mnist数据集训练3个epoch后test accuracy(98%)。
+- 实验效果查看[net-test-mnist.ipynb](https://github.com/heyxhh/nnet-numpy/blob/master/net-test-mnist.ipynb)
--- optim.py
+++ optim.py
@@ -1,18 +1,37 @@
+import numpy as np
+
 class SGD():
     """
     随机梯度下降
+    parameters: 模型需要训练的参数
+    lr: float, 学习率
+    momentum: float, 动量因子，默认为None不使用动量梯度下降
     """
-    def __init__(self, parameters, lr):
-        """
-        parameters：模型需要训练的参数
-        lr：学习率
-        """
+    def __init__(self, parameters, lr, momentum=None):
         self.parameters = parameters
         self.lr = lr
-    
+        self.momentum = momentum
+
+        if momentum is not None:
+            self.velocity = self.velocity_initial()
+
     def update_parameters(self, grads):
         """
         grads: 调用network的backward方法，返回的grads.
         """
-        for param, grad in zip(self.parameters, grads):
-            param -= self.lr * grad
+        if self.momentum == None:
+            for param, grad in zip(self.parameters, grads):
+                param -= self.lr * grad
+        else:
+            for i in range(len(self.parameters)):
+                self.velocity[i] = self.momentum * self.velocity[i] - self.lr * grads[i]
+                self.parameters[i] += self.velocity[i]
+    
+    def velocity_initial(self):
+        """
+        初始化velocity，按照parameters的参数顺序依次将v初始化为0
+        """
+        velocity = []
+        for param in self.parameters:
+            velocity.append(np.zeros_like(param))
+        return velocity
